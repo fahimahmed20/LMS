45227277e95ee1af6b1db10210c71a0ffb9aaf9f
fahimahmed20 <this.fahimahmed@gmail.com>
2023-01-14 01:03:52 +0600

    update

--- student/sign-in.php
+++ student/sign-in.php
@@ -1,12 +1,30 @@
+<?php
+require_once("../dbcon.php");
+if(isset($_POST['login_submit'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+   $result =  mysqli_query($con, "SELECT * FROM `students` WHERE `email` = '$email' OR `username` = '$email';");
+   $result_err = mysqli_num_rows($result);
+   if($result_err == 1){
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password,$row['password'])){
+            header('Location: index.php');
+            exit;
+        }else{
+            $resss_error = "Password invalid";
+        }
+   }else{
+        $resss_error = "Username or email invalid";
+   }
+}
+?>
 <!doctype html>
 <html lang="en" class="fixed accounts sign-in">
-
-
-<!-- Mirrored from myiideveloper.com/helsinki/last-version/helsinki_green-dark/src/pages_sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 05 Mar 2019 13:05:33 GMT -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-    <title>Helsinki</title>
+    <title>Sign-In</title>
     <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
     <link rel="icon" type="image/png" sizes="192x192" href="favicon/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
@@ -32,21 +50,33 @@
         <!--LOGO-->
         <div class="logo">
             <h1 class="text-center">LMS</h1>
+            <?php
+            if(isset($resss_error)){
+                ?>
+                <div class="alert alert-danger" role="alert">
+                    <span style="color: #fff;"><?= $resss_error; ?></span>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true" style="color: #fff;">&times;</span>
+                    </button>
+                </div>
+                <?php
+            }
+            ?>
         </div>
         <div class="box">
             <!--SIGN IN FORM-->
             <div class="panel mb-none">
                 <div class="panel-content bg-scale-0">
-                    <form>
+                    <form method="post" action="<?= $_SERVER['PHP_SELF'];?>">
                         <div class="form-group mt-md">
                             <span class="input-with-icon">
-                                <input type="email" class="form-control" id="email" placeholder="Email">
+                                <input type="test" class="form-control" name="email" placeholder="Email / UserName" value="<?= isset($email) ? $email : ''; ?>">
                                 <i class="fa fa-envelope"></i>
                             </span>
                         </div>
                         <div class="form-group">
                             <span class="input-with-icon">
-                                <input type="password" class="form-control" id="password" placeholder="Password">
+                                <input type="password" class="form-control" name="password" placeholder="Password">
                                 <i class="fa fa-key"></i>
                             </span>
                         </div>
@@ -57,13 +87,13 @@
                             </div>
                         </div>
                         <div class="form-group">
-                            <a href="index.html" class="btn btn-primary btn-block">Sign in</a>
+                            <input type="submit" value="Sign In" class="btn btn-primary btn-block" name="login_submit">
                         </div>
                         <div class="form-group text-center">
                             <a href="pages_forgot-password.html">Forgot password?</a>
                             <hr/>
                              <span>Don't have an account?</span>
-                            <a href="pages_register.html" class="btn btn-block mt-sm">Register</a>
+                            <a href="register.php" class="btn btn-block mt-sm">Register</a>
                         </div>
                     </form>
                 </div>
@@ -84,7 +114,4 @@
 <!-- SECTION script and examples-->
 <!-- ========================================================= -->
 </body>
-
-
-<!-- Mirrored from myiideveloper.com/helsinki/last-version/helsinki_green-dark/src/pages_sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 05 Mar 2019 13:05:37 GMT -->
 </html>
